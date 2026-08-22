98e423dd1de8973b7345aaf45d04e6bd1dc5a3b2
Arya N M <aryanm026@gmail.com>
2026-08-23 02:44:30 +0530

    BUG FIX

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\task\TaskRequest;
+use App\Http\Requests\Task\TaskRequest;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
